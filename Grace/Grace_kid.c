5c67dd657f7902d90cc83c381ee0de3d2856cd06
Cedric Poulet <cpoulet@student.42.fr>
2017-09-12 15:12:36 +0200

    removing from git

--- Grace/Grace_kid.c
+++ Grace/Grace_kid.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <fcntl.h>
 #define NL 10
-#define GRACE "#include <stdio.h>%c#define NL 10%c#define GRACE %c%s%c%c#define FT(x,y) int main(){fprintf(fopen(%cGrace_kid.c%c, %cw%c),GRACE,x,x,y,GRACE,y,x,y,y,y,y,x,x,x,x,x);return(0);}%c/*%cLa fleur en bouquet fane, et jamais ne renaît !%c*/%cFT(NL,34)%c"
-#define FT(x,y) int main(){fprintf(fopen("Grace_kid.c", "w"),GRACE,x,x,y,GRACE,y,x,y,y,y,y,x,x,x,x,x);return(0);}
+#define GRACE "#include <stdio.h>%c#include <fcntl.h>%c#define NL 10%c#define GRACE %c%s%c%c#define FT(x,y) int main(){int fd=open(%cGrace_kid.c%c,O_WRONLY|O_CREAT|O_TRUNC,S_IRWXU);dprintf(fd,GRACE,x,x,x,y,GRACE,y,x,y,y,x,x,x,x,x);return(0);}%c/*%cLa fleur en bouquet fane, et jamais ne renaît !%c*/%cFT(NL,34)%c"
+#define FT(x,y) int main(){int fd=open("Grace_kid.c",O_WRONLY|O_CREAT|O_TRUNC,S_IRWXU);dprintf(fd,GRACE,x,x,x,y,GRACE,y,x,y,y,x,x,x,x,x);return(0);}
 /*
 La fleur en bouquet fane, et jamais ne renaît !
 */
