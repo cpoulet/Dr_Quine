0322edc247b0ab678c094714dd36c4557a28e713
Cedric Poulet <cpoulet@student.42.fr>
2017-09-12 15:50:53 +0200

    Third Quine ok !

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#define SULLY "#include <stdio.h>%c#include <unistd.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];%cif(i<=0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);%cfprintf(fopen(str,%cw%c),SULLY,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,34,34,10,10,10);%creturn(0);%c}%c"
+#include <fcntl.h>
+#define SULLY "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];%cif(i<=0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);%cint fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,10,10,10,34,37,34,34,37,37,34,10,10,10,10);close(fp);%cchar bin[16];char cmd[50];%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%cgcc %cs -o %cs%c,str,bin);%csystem(cmd);execv(bin,NULL);%creturn(0);%c}%c"
 int main()
 {
-int i = 1;
+int i = 5;
 char str[16];
 if(i<=0)return(0);
 sprintf(str,"Sully_%d.c",--i);
-FILE *fp;
-fp = fopen(str,"w");
-fprintf(fp,SULLY,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,34,34,10,10,10);
-fclose(fp);
-char *args[] = {"gcc",str,"-o","test",0};
-execv("/usr/bin/gcc", args);
-printf("HELLO WORLD\n");
+int fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);
+dprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,10,10,10,34,37,34,34,37,37,34,10,10,10,10);close(fp);
+char bin[16];char cmd[50];
+sprintf(bin,"Sully_%d",i);sprintf(cmd,"gcc %s -o %s",str,bin);
+system(cmd);execv(bin,NULL);
 return(0);
 }
