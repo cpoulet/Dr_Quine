d193d9b317d59e9c34b8fa67bd1c0e4b3bc30490
Cedric Poulet <cpoulet@student.42.fr>
2017-09-12 12:55:59 +0200

    Third in progress

---
+++ Sully/Sully.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#define SULLY "#include <stdio.h>%c#include <unistd.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];%cif(i<=0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);%cfprintf(fopen(str,%cw%c),SULLY,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,34,34,10,10,10);%creturn(0);%c}%c"
+int main()
+{
+int i = 1;
+char str[16];
+if(i<=0)return(0);
+sprintf(str,"Sully_%d.c",--i);
+fprintf(fopen(str,"w"),SULLY,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,10,34,34,10,10,10);
+char*args[] = {"-o","test", 0};
+execv("usr/bin/gcc", args);
+return(0);
+}
