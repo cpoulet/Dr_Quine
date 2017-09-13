91318a3ee15c9da3e8fb870951a51cbde8fabff1
Cedric Poulet <cpoulet@student.42.fr>
2017-09-13 19:06:58 +0200

    Debug wc -l =13

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define SULLY "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];char cmd[100];%cif(i<=0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;%c,str,str);system(cmd);%cint fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10,10);close(fp);%cchar bin[16];%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;gcc %cs -o %cs%c,bin,bin,str,bin);%csystem(cmd);execv(bin,NULL);%creturn(0);%c}%c"
+#define SULLY "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];char cmd[100];%cif(i<0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;%c,str,str);system(cmd);%cint fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10,10);close(fp);%cchar bin[16];%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;gcc %cs -o %cs%c,bin,bin,str,bin);%csystem(cmd);execv(bin,NULL);%creturn(0);%c}%c"
 int main()
 {
 int i = 5;
 char str[16];char cmd[100];
-if(i<=0)return(0);
+if(i<0)return(0);
 sprintf(str,"Sully_%d.c",--i);sprintf(cmd,"[ -e %s ] && rm -f %s;",str,str);system(cmd);
 int fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);
 dprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10,10);close(fp);
