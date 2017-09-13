f9eb7fcc2c7e0b22447ad5d14f8a87fbc1670ff8
Cedric Poulet <cpoulet@student.42.fr>
2017-09-13 14:08:54 +0200

    Grace in python ok

--- Bonus/Grace/Grace.py
+++ Bonus/Grace/Grace.py
@@ -1,2 +1,7 @@
-s = 's = {}\nprint(s.format(repr(s)))'
-print(s.format(repr(s)))
+# Welcome your Grace !
+
+FILE = 'Grace_kid.py'
+STRING = "# Welcome your Grace !\n\nFILE = 'Grace_kid.py'\nSTRING = {}\nCREATE = lambda X: print(X.format(repr(X)),file=open(FILE, 'w'))\n\nCREATE(STRING)"
+CREATE = lambda X: print(X.format(repr(X)),file=open(FILE, 'w'))
+
+CREATE(STRING)
