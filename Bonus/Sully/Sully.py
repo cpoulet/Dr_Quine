e044e4ef1eee03d2d7dcdf1a4579c50131035d35
Cedric POULET <cpoulet@student.42.fr>
2017-11-03 15:58:51 +0100

    Sully perfect

--- Bonus/Sully/Sully.py
+++ Bonus/Sully/Sully.py
@@ -3,10 +3,10 @@ INT = 5
 NAME = 'Sully_'
 FILE = lambda N: NAME + str(N)
 FNAME = lambda N: FILE(N) + '.py'
-STRING = "import os\nINT = {}\nNAME = 'Sully_'\nFILE = lambda N: NAME + str(N)\nFNAME = lambda N: FILE(N) + '.py'\nSTRING = {}\nCREATE = lambda X,N: print(X.format(N,repr(X)),file=open(FNAME(N), 'w'))\n\nif INT >= 0:\n    INT -= 1\n    CREATE(STRING,INT)\n    os.system('python3 ' + FNAME(INT))"
+STRING = "import os\nINT = {}\nNAME = 'Sully_'\nFILE = lambda N: NAME + str(N)\nFNAME = lambda N: FILE(N) + '.py'\nSTRING = {}\nCREATE = lambda X,N: print(X.format(N,repr(X)),file=open(FNAME(N), 'w'))\n\nif INT > 0:\n    CREATE(STRING,INT)\n    CREATE(STRING,INT-1)\n    os.system('python3 ' + FNAME(INT-1))"
 CREATE = lambda X,N: print(X.format(N,repr(X)),file=open(FNAME(N), 'w'))
 
-if INT >= 0:
-    INT -= 1
+if INT > 0:
     CREATE(STRING,INT)
-    os.system('python3 ' + FNAME(INT))
+    CREATE(STRING,INT-1)
+    os.system('python3 ' + FNAME(INT-1))
--- src/Sully.c
+++ src/Sully.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define SULLY "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar str[16];char cmd[100];%cif(i<0)return(0);%csprintf(str,%cSully_%cd.c%c,--i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;%c,str,str);system(cmd);%cint fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10,10);close(fp);%cchar bin[16];%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;gcc %cs -o %cs%c,bin,bin,str,bin);%csystem(cmd);execv(bin,NULL);%creturn(0);%c}%c"
+#define ARG fp,SULLY,10,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,34,37,34,34,37,37,37,37,34,10,34,37,34,34,37,37,34,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10
+#define SULLY "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c#define ARG fp,SULLY,10,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,34,37,34,34,37,37,37,37,34,10,34,37,34,34,37,37,34,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10%c#define SULLY %c%s%c%cint main()%c{%cint i = %d;%cchar fname[16];char cmd[100];char bin[16];%cif(i<1)return(0);%csprintf(fname,%cSully_%cd.c%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;%c,fname,fname);system(cmd);%cint fp=open(fname,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(ARG);close(fp);%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;gcc %cs -o %cs%c,bin,bin,fname,bin);system(cmd);%csprintf(fname,%cSully_%cd.c%c,--i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;%c,fname,fname);system(cmd);%cfp=open(fname,O_WRONLY|O_CREAT|O_TRUNC,0777);%cdprintf(ARG);close(fp);%csprintf(bin,%cSully_%cd%c,i);sprintf(cmd,%c[ -e %cs ] && rm -f %cs;gcc %cs -o %cs%c,bin,bin,fname,bin);system(cmd);execv(bin,NULL);%creturn(0);%c}%c"
 int main()
 {
-int i = 10;
-char str[16];char cmd[100];
-if(i<0)return(0);
-sprintf(str,"Sully_%d.c",--i);sprintf(cmd,"[ -e %s ] && rm -f %s;",str,str);system(cmd);
-int fp=open(str,O_WRONLY|O_CREAT|O_TRUNC,0777);
-dprintf(fp,SULLY,10,10,10,10,34,SULLY,34,10,10,10,i,10,10,10,34,37,34,34,37,37,34,10,10,10,10,34,37,34,34,37,37,37,37,34,10,10,10,10);close(fp);
-char bin[16];
-sprintf(bin,"Sully_%d",i);sprintf(cmd,"[ -e %s ] && rm -f %s;gcc %s -o %s",bin,bin,str,bin);
-system(cmd);execv(bin,NULL);
+int i = 5;
+char fname[16];char cmd[100];char bin[16];
+if(i<1)return(0);
+sprintf(fname,"Sully_%d.c",i);sprintf(cmd,"[ -e %s ] && rm -f %s;",fname,fname);system(cmd);
+int fp=open(fname,O_WRONLY|O_CREAT|O_TRUNC,0777);
+dprintf(ARG);close(fp);
+sprintf(bin,"Sully_%d",i);sprintf(cmd,"[ -e %s ] && rm -f %s;gcc %s -o %s",bin,bin,fname,bin);system(cmd);
+sprintf(fname,"Sully_%d.c",--i);sprintf(cmd,"[ -e %s ] && rm -f %s;",fname,fname);system(cmd);
+fp=open(fname,O_WRONLY|O_CREAT|O_TRUNC,0777);
+dprintf(ARG);close(fp);
+sprintf(bin,"Sully_%d",i);sprintf(cmd,"[ -e %s ] && rm -f %s;gcc %s -o %s",bin,bin,fname,bin);system(cmd);execv(bin,NULL);
 return(0);
 }
